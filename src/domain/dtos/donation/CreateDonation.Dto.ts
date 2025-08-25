64e997febb60ee1b15c8c5509d4f81880e9b92e5
Ricardo Figuera <ricardofiguera.c@gmail.com>
2025-08-25 16:58:37 +0200

    fix: corregir nombres de propiedades y mejorar la lógica de carga de imágenes en el controlador

--- src/domain/dtos/donation/CreateDonation.Dto.ts
+++ src/domain/dtos/donation/CreateDonation.Dto.ts
@@ -1,5 +1,5 @@
 import { IsString, IsNumber, IsOptional, IsBoolean } from "class-validator";
-import { Type } from "class-transformer";
+import { Transform, Type } from "class-transformer";
 
 export class CreateDonationDto {
   @IsString()
@@ -20,13 +20,13 @@ export class CreateDonationDto {
   @IsNumber()
   latitude!: number;
 
-  @Type(() => Number)
   @IsNumber()
+  @Type(() => Number)
   longitude!: number;
 
-  @Type(() => Boolean)
   @IsBoolean()
-  urgency!: boolean;
+  @Type(() => Boolean)
+  urgent!: boolean;
 
   @IsOptional()
   @Type(() => Date)
