8e5cf96f39de2878acfdae88b613c418e3da4aac
BrionesDev <elianbrionesvictores65@gmail.com>
2025-08-28 16:16:55 +0200

    fix: Improvement Chat model

--- src/domain/entities/Chat.ts
+++ src/domain/entities/Chat.ts
@@ -2,6 +2,8 @@ export class Chat {
 	constructor(
 		public id: number,
 		public userId: string,
+		public isRead: boolean = false,
+		public lastMessage: string,
 		public createdAt?: Date,
 		public updatedAt?: Date
 	) {}
@@ -10,6 +12,8 @@ export class Chat {
 		return new Chat(
 			userData.id,
 			userData.userId,
+			userData.isRead,
+			userData.lastMessage,
 			userData.createdAt,
 			userData.updatedAt
 		);
@@ -19,6 +23,8 @@ export class Chat {
 		return {
 			id: this.id,
 			userId: this.userId,
+			isRead: this.isRead,
+			lastMessage: this.lastMessage,
 			createdAt: this.createdAt,
 			updatedAt: this.updatedAt,
 		};
