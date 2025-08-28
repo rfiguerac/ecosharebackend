b1771dbdfc44308f9370c1730c4c832a00f88d81
BrionesDev <elianbrionesvictores65@gmail.com>
2025-08-28 18:08:48 +0200

    fix: Small change on Socket Server in send_messages

--- src/app.ts
+++ src/app.ts
@@ -17,52 +17,56 @@ function main() {
   server.start();
 
   // IMPORTANT: Seed data for testing purposes, REMOVE when finish development
-  (async () => {
-    await prisma.user.createMany({
-      data: [
-        {
-          name: "Elian",
-          email: "elian@e.com",
-          password: "123456",
-        },
-        {
-          name: "Ricardo",
-          email: "ricardo@r.com",
-          password: "654321",
-        },
-      ],
-    });
+  // (async () => {
+  //   await prisma.user.deleteMany();
+  //   await prisma.chat.deleteMany();
+  //   await prisma.chatMessage.deleteMany();
 
-    await prisma.chat.createMany({
-      data: [
-        {
-          userId: 1,
-          isRead: false,
-          lastMessage: "only test",
-        },
-        {
-          userId: 2,
-          isRead: false,
-          lastMessage: "second test",
-        },
-      ],
-    });
+  //   await prisma.user.createMany({
+  //     data: [
+  //       {
+  //         name: "Elian",
+  //         email: "elian@e.com",
+  //         password: "123456",
+  //       },
+  //       {
+  //         name: "Ricardo",
+  //         email: "ricardo@r.com",
+  //         password: "654321",
+  //       },
+  //     ],
+  //   });
 
-    await prisma.chatMessage.createMany({
-      data: [
-        {
-          chatId: 1,
-          senderId: 1,
-          receiverId: 2,
-          message: "Hello, how are you?",
-        },
-        {
-          chatId: 1,
-          senderId: 2,
-          receiverId: 1,
-          message: "I'm fine, thank you!",
-        },
-      ],
-    });
-  })();
+  //   await prisma.chat.createMany({
+  //     data: [
+  //       {
+  //         userId: 1,
+  //         isRead: false,
+  //         lastMessage: "only test",
+  //       },
+  //       {
+  //         userId: 2,
+  //         isRead: false,
+  //         lastMessage: "second test",
+  //       },
+  //     ],
+  //   });
+
+  //   await prisma.chatMessage.createMany({
+  //     data: [
+  //       {
+  //         chatId: 1,
+  //         senderId: 1,
+  //         receiverId: 2,
+  //         message: "Hello, how are you?",
+  //       },
+  //       {
+  //         chatId: 1,
+  //         senderId: 2,
+  //         receiverId: 1,
+  //         message: "I'm fine, thank you!",
+  //       },
+  //     ],
+  //   });
+  // })();
 }
