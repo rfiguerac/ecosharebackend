a89034f3c88b22b2ffcc47818e72378c0834379d
Ricardo Figuera <ricardofiguera.c@gmail.com>
2025-08-31 17:26:28 +0200

    refactor: Mejorar la legibilidad y formato del código en ChatMessage, ChatMessageDataSourceImpl, ChatMessageController y ChatMessageRouter

--- src/presentation/chat/ChatMessageController.ts
+++ src/presentation/chat/ChatMessageController.ts
@@ -1,27 +1,33 @@
 import { Request, Response } from "express";
-import { ChatMessageRepository, GetMessagesByChatId, SendMessage } from "../../domain";
+import {
+  ChatMessageRepository,
+  GetMessagesByChatId,
+  SendMessage,
+} from "../../domain";
 
 export class ChatMessageController {
-    constructor(private readonly userRepository: ChatMessageRepository) {}
+  constructor(private readonly userRepository: ChatMessageRepository) {}
 
-    getMessagesByChatId = async (req: Request, res: Response) => {
-        try {
-            const chat = await new GetMessagesByChatId(this.userRepository).execute(Number(req.params.id));
-            if (!chat) {
-                return res.status(404).json({ message: "Chat not found" });
-            }
-            res.status(200).json(chat);
-        } catch (error) {
-            res.status(500).json({ message: "Internal server error" });
-        }
-    };
+  getMessagesByChatId = async (req: Request, res: Response) => {
+    try {
+      const chat = await new GetMessagesByChatId(this.userRepository).execute(
+        Number(req.params.id)
+      );
+      if (!chat) {
+        return res.status(404).json({ message: "Chat not found" });
+      }
+      res.status(200).json(chat);
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  };
 
-    sendMessage = async (req: Request, res: Response) => {
-        try {
-            const chat = await new SendMessage(this.userRepository).execute(req.body);
-            res.status(201).json(chat);
-        } catch (error) {
-            res.status(500).json({ message: "Internal server error" });
-        }
-    };
+  sendMessage = async (req: Request, res: Response) => {
+    try {
+      const chat = await new SendMessage(this.userRepository).execute(req.body);
+      res.status(201).json(chat);
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  };
 }
--- src/presentation/chat/ChatMessageRouter.ts
+++ src/presentation/chat/ChatMessageRouter.ts
@@ -1,19 +1,28 @@
 import { Router } from "express";
-import { ChatMessageRepositoryImpl, ChatMessageDataSourceImpl } from "../../infrastructure";
+import {
+  ChatMessageRepositoryImpl,
+  ChatMessageDataSourceImpl,
+} from "../../infrastructure";
 import { ChatMessageController } from "./ChatMessageController";
 import { validateDto } from "../../middleware";
 import { SendMessageDto } from "../../domain";
 
 export class ChatMessageRouter {
-    public static router(): Router {
-        const router = Router();
-        const datasource = new ChatMessageDataSourceImpl();
-        const chatMessageRepository = new ChatMessageRepositoryImpl(datasource);
-        const chatMessageController = new ChatMessageController(chatMessageRepository);
+  public static router(): Router {
+    const router = Router();
+    const datasource = new ChatMessageDataSourceImpl();
+    const chatMessageRepository = new ChatMessageRepositoryImpl(datasource);
+    const chatMessageController = new ChatMessageController(
+      chatMessageRepository
+    );
 
-        router.get("/:chatId/messages", chatMessageController.getMessagesByChatId);
-        router.post("/send-message", validateDto(SendMessageDto), chatMessageController.sendMessage);
+    router.get("/:id/messages", chatMessageController.getMessagesByChatId);
+    router.post(
+      "/send-message",
+      validateDto(SendMessageDto),
+      chatMessageController.sendMessage
+    );
 
-        return router;
-    }
+    return router;
+  }
 }
